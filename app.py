b38bd23b49be6b313739b3d7665276b422dc3901
Rashiq <rashiq.z@gmail.com>
2017-05-02 02:41:06 +0200

    Add signatuer checking

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
-from flask import Flask, request
 import json
+import hashlib
+from flask import Flask, request
+
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['POST'])
 def hook():
-  return json.dumps(request.form)
+  if not check_signature(): abort(403)
+  repository = request.get_json()['repository']['name']
+  return ('', 204)
+
+
+def check_signature(request):
+  token = os.environ['GITHUB_TOKEN']
+  digest = 'sha1=' + hashlib.sha1(token).hexdigest()
+  signature request.headers.get('X-Hub-Signature')
+  return digest == signature
 
 
 if __name__ == '__main__':
